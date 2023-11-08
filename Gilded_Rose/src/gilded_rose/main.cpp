0a2548cb5abdb86aeccf3af8149497731a10be84
Junior Dongmo Nguemo <junior.dongmonguemo@stud.h-da.de>
2023-11-08 23:48:22 +0100

    space added between 'Sulfura' and the comma.Sulfuras changed to Sulfurs

--- Gilded_Rose/src/gilded_rose/main.cpp
+++ Gilded_Rose/src/gilded_rose/main.cpp
@@ -13,7 +13,7 @@ int main()
   items.push_back(G.createItem("+5 Dexterity Vest", 10, 20));
   items.push_back(G.createItem("Aged Brie", 2, 0));
   items.push_back(G.createItem("Elixir of the Mongoose", 5, 7));
-  items.push_back(G.createItem("Sulfuras, Hand of Ragnaros", 0, 80));
+  items.push_back(G.createItem("Sulfurs , Hand of Ragnaros", 0, 80));
   items.push_back(G.createItem("Sulfuras, Hand of Ragnaros", -1, 80));
   items.push_back(G.createItem("Backstage passes to a TAFKAL80ETC concert", 15, 20));
   items.push_back(G.createItem("Backstage passes to a TAFKAL80ETC concert", 10, 49));
