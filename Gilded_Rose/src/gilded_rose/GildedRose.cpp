4425d982a4fb6400e3fd3e9a55e0bcda174c5375
Junior Dongmo Nguemo <junior.dongmonguemo@stud.h-da.de>
2023-11-20 22:44:30 +0100

    complete resolution of lab1 with use of refactoring.

--- Gilded_Rose/src/gilded_rose/GildedRose.cpp
+++ Gilded_Rose/src/gilded_rose/GildedRose.cpp
@@ -1,6 +1,9 @@
 #include "GildedRose.h"
 
+ const int Max_Quality = 50;
+ const int Min_Quality = 0;
 GildedRose::GildedRose(vector<Item *> &items)
+    
     : items(items) {}
 
 GildedRose::~GildedRose() {
@@ -8,59 +11,71 @@ GildedRose::~GildedRose() {
     delete item;
   }
 }
+void GildedRose:: updatesellIn(Item* item){
+  item->sellIn -= 1;
+}
+void GildedRose::updateQuality_ForAgedBrie(Item* item){
+  if(item->quality< Max_Quality && item->sellIn> Min_Quality){
+    item->quality+=1;
+  }else if(item->quality<=48 && item->sellIn<= Min_Quality){
+    item->quality += 2;
+  }
+  updatesellIn (item);
+}
 
-void GildedRose::updateQuality() {
-  for (int i = 0; i < items.size(); i++) {
-    Item *&pItem = items[i];
-
-    if (pItem->name != "Aged Brie" && pItem->name != "Backstage passes to a TAFKAL80ETC concert") {
-      if (pItem->quality > 0) {
-        if (pItem->name != "Sulfuras, Hand of Ragnaros") {
-          pItem->quality = pItem->quality - 1;
-        }
-      }
-    } else {
-      if (pItem->quality < 50) {
-        pItem->quality = pItem->quality + 1;
-
-        if (pItem->name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (pItem->sellIn <= 10) {
-            if (pItem->quality < 50) {
-              pItem->quality = pItem->quality + 1;
-            }
-          }
-
-          if (pItem->sellIn <= 5) {
-            if (pItem->quality < 50) {
-              pItem->quality = pItem->quality + 1;
-            }
-          }
-        }
-      }
+void GildedRose::updateQuality_ForBackstagePasses(Item* item){
+  if(item->sellIn> Min_Quality){
+    if(item->sellIn<=10 && item->sellIn>5){
+      item->quality+=2;
+    }else if(item->sellIn<=5){
+      item->quality+=3;
+    }else{
+      item->quality++;
     }
+  }else{
+    item->quality= Min_Quality;
+  }
+  updatesellIn(item);
+}
 
-    if (pItem->name != "Sulfuras, Hand of Ragnaros") {
-      pItem->sellIn = pItem->sellIn - 1;
-    }
+void GildedRose::updateQuality_ForConjuredManacake(Item* item){
+  // conjured Artikel verlieren doppekt so schnell an Qualität wie normale Artikel
+  if(item->quality> Min_Quality){
+    item->quality-=2;
+  }
+  updatesellIn(item);
+}
+void GildedRose::updateQuality_ForNormalItem(Item* item){
+   if(item->quality> Min_Quality){
+    item->quality -=1;
+   }
+   updatesellIn(item);
+}
+void GildedRose::updateQuality(){
+  for (auto & pItem : items) {// using range-based
+     if (pItem->typeOf()== EXPIRING){
+      updateQuality_ForBackstagePasses(pItem);
+     }else if(pItem->typeOf()== MATURING){
+      updateQuality_ForAgedBrie(pItem);
+     }else if(pItem->typeOf()==CONJURED){
+      updateQuality_ForConjuredManacake(pItem);
+     }else if(pItem->typeOf()==NORMAL){
+      updateQuality_ForNormalItem(pItem);
+     }else if(pItem->typeOf()==LEGENDARY){
+      continue;
+     }
 
-    if (pItem->sellIn < 0) {
-      if (pItem->name != "Aged Brie") {
-        if (pItem->name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (pItem->quality > 0) {
-            if (pItem->name != "Sulfuras, Hand of Ragnaros") {
-              pItem->quality = pItem->quality - 1;
-            }
-          }
-        } else {
-          pItem->quality = pItem->quality - pItem->quality;
-        }
-      } else {
-        if (pItem->quality < 50) {
-          pItem->quality = pItem->quality + 1;
-        }
-      }
-    }
+     // sicherstellen ,dass die  Qualität nicht negativ wird
+     if(pItem->quality< Min_Quality){
+      pItem->quality= Min_Quality;
+     }
+
+     // Sicherstellen ,dass die Qualität nicht über 50 steigt (ausser bei" sulfuras" )
+     if(pItem->quality > Max_Quality && pItem->typeOf()!= LEGENDARY){
+      pItem->quality= Max_Quality;
+     }
   }
 }
+
 
 Item *GildedRose::createItem(string name, int sellIn, int quality) const { return new Item(name, sellIn, quality); }
--- Gilded_Rose/src/gilded_rose/GildedRose.h
+++ Gilded_Rose/src/gilded_rose/GildedRose.h
@@ -1,6 +1,8 @@
 #include "Item.h"
 #include <string>
 #include <vector>
+#include "ItemTypes.h"
+
 
 using namespace std;
 
@@ -11,4 +13,9 @@ public:
   Item *createItem(string, int, int) const;
   void updateQuality();
   virtual ~GildedRose();
+  static void updateQuality_ForAgedBrie(Item *item);
+  static void updatesellIn(Item *item);
+  static void updateQuality_ForBackstagePasses(Item *item);
+  static void updateQuality_ForConjuredManacake(Item *item);
+  static void updateQuality_ForNormalItem(Item *item);
 };
